da62165122f30b2184e73256d069bc7b2235b76d
Kate Wannell <katefwannell7@gmail.com>
2017-02-19 17:25:39 -0800

    checkboxes and looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,20 @@
 
-var UserInputStatus = function(fieldName,fieldValue){
-
-	if (fieldValue === "") {
-		alert ("Enter a number to count " + fieldName);
-	} else if (isNaN(fieldValue))  {
-		alert ("Enter a valid number to count " + fieldName);
-	}
-}
-
 $(document).ready(function() {
 
-	$("#loopUser form").submit(function(event) {
+	$("form#transportation_survey").submit(function(event) {
 		event.preventDefault();
 
-		UserInputStatus("from",$("#countFrom").val());
-		UserInputStatus("to",$("#countTo").val());
-	  UserInputStatus("by",$("#countBy").val());
+		$("#work-responses").show();
+		$("input:checkbox[name=work-transportation]:checked").each(function(){
+			$("#work-responses").append($(this).val() + "<br>");
+		});
 
-    var countFrom = parseInt($("#countFrom").val());
-		var countTo = parseInt($("#countTo").val());
-		var countBy = parseInt($("#countBy").val());
+		$("#fun-responses").show();
+		$("input:checkbox[name=fun-transportation]:checked").each(function(){
+			$("#fun-responses").append($(this).val() + "<br>");
+		});
 
-		for (var index = countFrom; index <= countTo; index += countBy ) {
-			$(".resultUser").append($("<li>").text(index));
-		}
-	});
+		$("#transportation_survey").hide();
 
-	$("#loop30 form").submit(function(event) {
-		event.preventDefault();
-		for (var index = 5; index <= 30; index += 5 ) {
-			$(".result30").append($("<li>").text(index));
-		}
-	});
-
-	$("#loop50 form").submit(function(event) {
-		event.preventDefault();
-		for (var index = 7; index <= 50; index += 7 ) {
-			$(".result50").append($("<li>").text(index));
-		}
 	});
 });
